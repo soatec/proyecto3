8619f95585893197a49c05955e3dca06d952456f
Daniel <danielac1794@gmail.com>
2020-06-25 01:24:38 -0700

    Add get vehicle image using vehicle data (type, color, direction); Add missing bus sprites; Add missing ambulance sprites.

--- include/user_interface.h
+++ include/user_interface.h
@@ -2,6 +2,7 @@
 #define PROYECTO3_USER_INTERFACE_H
 
 #include "threadville_types.h"
+#include <SDL2/SDL_surface.h>
 
 /**
  * Init sdl needed components
@@ -18,5 +19,20 @@ void destroy_ui();
  * Update and draw
  */
 void core_loop();
+
+/**
+ * Load vehicle images into memory
+ */
+void load_vehicle_images();
+
+/**
+ * Free vehicle images from memory
+ */
+void free_vehicle_images();
+
+/**
+ * Get vehicle image with proper type, color and orientation for a vehicle
+ */
+SDL_Surface* get_vehicle_image(vehicle_data_t *vehicle);
 
 #endif //PROYECTO3_USER_INTERFACE_H
