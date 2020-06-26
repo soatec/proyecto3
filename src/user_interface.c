3f6cbe25e24fcd3929def777090101e0cd7468f8
Kenneth Paniagua <kpaniagua@hpe.com>
2020-06-25 23:05:44 -0600

    Fix initial direction

--- src/user_interface.c
+++ src/user_interface.c
@@ -184,12 +184,15 @@ void disable_white_bus(){
 void core_loop() {
     bool keep_window_open = true;
 
-    //create_new_bus(WHITE);
-    //create_new_bus(GRAY);
-    //create_new_bus(BLACK);
-    //create_new_bus(PINK);
-    //create_new_bus(LIGHT_BLUE);
-    //create_new_bus(ORANGE);
+    create_new_bus(RED);
+    create_new_bus(GREEN);
+    create_new_bus(BLUE);
+    create_new_bus(WHITE);
+    create_new_bus(GRAY);
+    create_new_bus(BLACK);
+    create_new_bus(PINK);
+    create_new_bus(LIGHT_BLUE);
+    create_new_bus(ORANGE);
     //create_new_random_car();
     //create_new_random_car();
     //create_new_random_car();
@@ -206,7 +209,6 @@ void core_loop() {
     //create_new_random_car();
     //create_new_random_car();
 
-    create_new_bus(RED);
 
 
 
